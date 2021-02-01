8f3c4a7af472b5c5cad44c889538614c7bf20dd0
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2021-02-01 17:36:45 +1100

    setup basic scss structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.jsx', './sass/app.scss'],
     output: {
         path: path.resolve(__dirname, '/dist'),
         publicPath: '/',
