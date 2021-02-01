0b5f1f43742de3debc0b77d42cd40de8a9a27145
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2021-02-01 17:14:53 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '/dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+        },
+        {
+           test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+           use: [
+             {
+               loader: 'url-loader'
+             },
+           ]
+         },
+         {  
+           test: /\.(woff|woff2|eot|ttf|otf)$/,
+           loader: "file-loader"
+         }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    devServer: {
+    //   index: index.html,
+      hot: true,
+    //   inline: true,
+      port: 8080,
+    //   historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
