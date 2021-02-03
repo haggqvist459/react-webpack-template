3fdf5317bd2493e4274b0d9ae0d2922070cdfe55
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2021-02-03 15:17:37 +1100

    changes to webpack & imports

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Router, Route, Switch, } from "react-router-dom";
+// import { PATHS } from './constants';
+
+import Home from './pages/Home';
+
+class App extends Component {
+    render() {
+        return (
+            <>TEST</>
+            // <Router>
+            //     <Switch>
+            //         <Route exact path={'/'} component={Home} />
+            //     </Switch>
+            // </Router>
+        )
+    }
+}
+
+export default App;
+
+// browser history
+// import { createBrowserHistory } from "history";
--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,59 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/index.jsx', './sass/app.scss'],
+    entry: ['./src/index.js', './sass/app.scss'],
     output: {
         path: path.resolve(__dirname, '/dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader",
-            ],
-        },
-        {
-           test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-           use: [
-             {
-               loader: 'url-loader'
-             },
-           ]
-         },
-         {  
-           test: /\.(woff|woff2|eot|ttf|otf)$/,
-           loader: "file-loader"
-         }
-      ]
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+                use: [
+                    {
+                        loader: 'url-loader'
+                    },
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: "file-loader"
+            }
+        ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx']
     },
     devServer: {
-    //   index: index.html,
-      hot: true,
-    //   inline: true,
-      port: 8080,
-    //   historyApiFallback: true,
+        //   index: index.html,
+        hot: true,
+        //   inline: true,
+        port: 8080,
+        //   historyApiFallback: true,
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./src/index.html"
         }),
-        
+
     ]
 };
