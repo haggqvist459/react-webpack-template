e540246215e6df06dc77a3386e08eb9b8774f936
haggqvist459 <tim.haggqvist@gmail.com>
2021-03-10 16:16:52 +1100

    added compression to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+
 const path = require('path');
+const zlib = require('zlib');
 
 module.exports = {
     entry: ['./src/index.js', './sass/app.scss'],
     output: {
-        path: path.resolve(__dirname, '/dist'),
-        publicPath: '/',
-        filename: 'app.bundle.js'
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -45,17 +51,63 @@ module.exports = {
         extensions: ['*', '.js', '.jsx']
     },
     devServer: {
-        port: 8083,
+        port: 8084,
         host: '0.0.0.0',
         hot: true,
         historyApiFallback: true,
         open: true,
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            chunks: "all",
+            minSize: 10000,
+            maxSize: 244000,
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./src/index.html",
+            favicon: './src/favicon.ico',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'assets/favicon', to: '' },
+            ],
 
+        }),
+        new CompressionPlugin({
+            filename: '[path][base].gz[query]',
+            algorithm: 'gzip',
+            test: /\.js$|\.css$||\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        }),
+        new CompressionPlugin({
+            filename: "[path][base].br[query]",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+                params: {
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                },
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false,
+        }),
     ]
 };
